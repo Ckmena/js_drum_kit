a21d9c86ff74d1ce2a6e684b1ce42339481b697b
Ckmena <c_menavalencia@fanshaweonline.ca>
2019-03-04 13:34:07 -0500

    transition added js

--- js/main.js
+++ js/main.js
@@ -18,7 +18,30 @@
 		audio.currentTime = 0;
 		audio.play();
 
+		//selct the parent div and animated
+		// try selcting the matching  parent div element
+		let key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+		key.classList.add('playing')
+
+	}
+
+	function removePlayingClass(e){
+		//listen for css transition to finish and then resetr the element
+		//by removing the playing class
+		
+		if (e.propertyName !== "transform") {
+			return;
+		} else {
+			console.log('do some stuff here, transition is done')
+
+		}
+	e.currentTarget.classList.remove("playing")
 	}
 
 	window.addEventListener('keydown', playDrumSound);
+
+	const key = Array.from(document.querySelectorAll('.key'));
+
+	key.forEach(key => key.addEventListener('transitionend', removePlayingClass));
+
 })();
